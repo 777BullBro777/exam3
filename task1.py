22525d5c2da3d1c5107489b1c7d3786b08de6f19
Kirilov Nikita <nikita.kirilov.02@mail.ru>
2021-11-30 17:27:09 +0300

    fix

--- task1.py
+++ task1.py
@@ -1,20 +1,17 @@
 # Напишите функцию, которая будет принимать номер кредитной карты и показывать только последние 4 цифры.
 # Остальные цифры должны заменяться звездочками
 
-def card_hide(card):
+def card_hide():
     try:
         card = input("Введите номер карты :  ")
     except ValueError:
         print(ValueError)
     else:
-        while True:
-            if card.isalpha():
-                print("Ошибка! Ввод нечисловых данных")
-                break
-            if card.isalnum():
-                return '*' * len(card[:-4]) + card[-4:]
+        if card.isalpha():
+            print("Ошибка! Ввод нечисловых данных")
+        if card.isdigit():
+            return '*' * len(card[:-4]) + card[-4:]
 
 
 if __name__ == "__main__":
-    card = None
-    print(card_hide(card))
+    print(card_hide())
